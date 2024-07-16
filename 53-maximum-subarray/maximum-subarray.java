48f49bfd149ed86eea64190539831970713bc601
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2024-07-16 00:53:46 -0700

    Time: 632 ms (6.68%) | Memory: 60.8 MB (7.04%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,141 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // divide and conquer method
+        /*
+        Boolean all_negative = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                all_negative = false;
+            }
+        }
+        if (all_negative) {
+            return Arrays.stream(nums).max().getAsInt();
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int q = nums.length / 2;
+        int[] left = new int[q];
+        int[] right = new int[nums.length - q];
+        for (int i = 0; i < q; i++) {
+            left[i] = nums[i];
+        }
+        for (int j = q; j < nums.length; j++) {
+            right[j - q] = nums[j];
+        }
+        int left_max = maxSubArray(left);
+        int right_max = maxSubArray(right);
+        int cross_left_max = 0;
+        int cross_right_max = 0;
+        int cross_current_sum_left = 0;
+        int cross_current_sum_right = 0;
+        for (int a = q - 1; a >= 0; a--) {
+            cross_current_sum_left = cross_current_sum_left + nums[a];
+            if (cross_current_sum_left > cross_left_max) {
+                cross_left_max = cross_current_sum_left;
+            }
+        }
+        for (int b = q; b < nums.length; b++) {
+            cross_current_sum_right = cross_current_sum_right + nums[b];
+            if (cross_current_sum_right > cross_right_max) {
+                cross_right_max = cross_current_sum_right;
+            }
+        }
+        return Math.max(Math.max(left_max, right_max), cross_left_max + cross_right_max);
+        */
+////////////////////
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        Boolean all_negative = true;
+        int[] max = new int[nums.length];
+        int max_sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                all_negative = false;
+            }
+            if (i == 0) {
+                max[i] = Math.max(0, nums[i]);
+                if (max[i] > max_sum) {
+                    max_sum = max[i];
+                }
+
+            } else {
+                max[i] = Math.max(0, nums[i] + max[i - 1]);
+                if (max[i] > max_sum) {
+                    max_sum = max[i];
+                }
+            }
+        }
+        Stack<Integer> indexes = new Stack<>();
+        for (int i=max.length-1; i>=0; i--){
+            if(max[i]==max_sum){
+                indexes.add(i);
+                for(int j=i-1; j>=0; j--){
+                    if(max[j] != 0){
+                        indexes.add(j);
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }
+
+        while(!indexes.isEmpty()) {
+            System.out.print(indexes.pop()+",");
+        }
+
+        if (all_negative) {
+            return Arrays.stream(nums).max().getAsInt();
+        } else {
+            // return Arrays.stream(max).max().getAsInt();
+            return max_sum;
+        }
+
+        
+
+        /*
+         * // Kanade's method
+         * 
+         * if (nums.length == 0)
+         * 
+         * {
+         * return 0;
+         * }
+         * if (nums.length == 1) {
+         * return nums[0];
+         * }
+         * Boolean all_negative = true;
+         * int[] max = new int[nums.length];
+         * int max_sum = 0;
+         * for (int i = 0; i < nums.length; i++) {
+         * if (nums[i] > 0) {
+         * all_negative = false;
+         * }
+         * if (i == 0) {
+         * max[i] = Math.max(0, nums[i]);
+         * } else {
+         * max[i] = Math.max(0, nums[i] + max[i - 1]);
+         * }
+         * }
+         * if (all_negative) {
+         * return Arrays.stream(nums).max().getAsInt();
+         * } else {
+         * return Arrays.stream(max).max().getAsInt();
+         * }
+         */
+        /// discard this block
+        /*
+         * for(int j=0;j<max.length; j++){
+         * if (max[j]> max_sum){
+         * max_sum=max[j];
+         * }
+         * }
+         * return max_sum;
+         */
+        //////////////////
+    }
+}
