cb83b9aa29e3b96ebd0c205a321502c0167ba46b
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2024-07-16 00:46:27 -0700

    Time: 0 ms (100%), Space: 41.8 MB (22.05%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -25,9 +25,18 @@ class Solution {
         //}
     }
     public boolean helper(TreeNode left, TreeNode right){
-        if(left == null || right == null){
-            return left==right;
+        if(left == null && right == null){
+            return true;
         }
+        if(left == null && right != null){
+            return false;
+        }
+        if(left != null && right == null){
+            return false;
+        }
+        /*if(left == null || right == null){
+            return left==right;
+        }*/
         if (left.val != right.val){
             return false;
         }
