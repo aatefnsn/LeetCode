4b9a875fda0e9dcf0a9fb65bdbe87e911e3af2cd
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2024-07-17 20:11:51 -0700

    Time: 5 ms (100.00%) | Memory: 41 MB (52.41%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -15,7 +15,45 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-       
+        if(root == null){
+            return null;
+        }else{
+            LinkedList<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            while(!q.isEmpty()){
+                //System.out.println(q.size());
+                TreeNode curr=q.pop();
+                System.out.println(curr.val);
+
+                if (curr.left !=null)
+                    q.add(curr.left);
+                if(curr.right != null)
+                    q.add(curr.right);
+
+                if(curr.left != null && curr.right !=null){
+                    TreeNode temp=curr.left;
+                    curr.left=curr.right;
+                    curr.right=temp;
+                    //q.add(q.peek().left);
+                    //q.add(q.pop().right);
+                }else if(curr.left == null && curr.right !=null){
+                    curr.left=curr.right;
+                    curr.right=null;
+                     //q.add(q.pop().right);
+                }else if(curr.left != null && curr.right ==null){
+                    curr.right=curr.left;
+                    curr.left=null;
+                     //q.add(q.pop().left);
+                }
+                
+            }
+            //
+        }
+        return root;
+    }
+}
+        //return root;
+       /*
     if (root == null){
         return root;
     }else{
@@ -28,24 +66,8 @@ class Solution {
         //}
         //if (root.right != null){
              invertTree(root.right);
-        //}
-        return root;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-}
+        // }
+        
+        //return root;
+        
+    }*/
