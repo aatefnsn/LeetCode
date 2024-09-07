2327cdc2cb7473531ed70c40cdb3c3908675f8c3
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2024-09-06 17:59:36 -0700

    Time: 0 ms (100.00%) | Memory: 43.1 MB (50.22%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -15,15 +15,16 @@ class Solution {
     }
 
     public int minCostClimbingStairs(int[] cost) {
-       int n = cost.length;
-	    int[] dp = new int[n];
-        for (int i=0; i<n; i++) {
-            if (i<2) dp[i] = cost[i];
-            else dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        int n=cost.length;
+        int[] dp = new int [n];
+        for(int i=0; i<n; i++){
+            if(i==0 || i ==1){
+                dp[i]=cost[i];
+            }else if (i >= 2){
+                dp[i]=cost[i]+Math.min(dp[i-1], dp[i-2]);
+            }
         }
-	    return Math.min(dp[n-1], dp[n-2]);
-        
-        
+        return Math.min(dp[n-1],dp[n-2]);
 
         // // recurrsive
         // if(cost.length ==0){
