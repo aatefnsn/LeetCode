01cc264999bc3120fdf30152699b28a1b8cf0489
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2024-09-10 18:45:24 -0700

    Time: 6 ms (7.46%) | Memory: 129.6 MB (95.54%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -3,71 +3,22 @@ class Solution {
         if (nums.length <3){
             return false;
         }else{
-            int n=nums.length;
-            int min=nums[0];
-            int max=nums[0];
-                
-            // track best sequence length  
-            // (not current sequence length) 
-            int seq = 1;         
-                
-            // min number in array 
-            int min_num = nums[0];   
-                
-            // least max number in best sequence  
-            // i.e. track arr[j] (e.g. in  
-            // array {1, 5, 3} our best sequence  
-            // would be {1, 3} with arr[j] = 3) 
-            int max_seq = Integer.MIN_VALUE; 
-            System.out.println("max seq is "+ max_seq);      
-                
-            // save arr[i] 
-            int store_min = min_num;    
-                
-            // Iterate from 1 to nums.size() 
-            for (int i = 1; i < nums.length; i++)  
-            { 
-                if (nums[i] == min_num) 
-                continue; 
-                
-                else if (nums[i] < min_num)  
-                { 
-                min_num = nums[i]; 
-                continue; 
-                }  
-                
-                // this condition is only hit  
-                // when current sequence size is 2 
-                else if (nums[i] < max_seq) {     
-                    System.out.println("kook"+nums[i]);
-                // update best sequence max number  
-                // to a smaller value  
-                // (i.e. we've found a  
-                // smaller value for arr[j]) 
-                max_seq = nums[i];        
-                    
-                // store best sequence start value  
-                // i.e. arr[i] 
-                store_min = min_num;             
-                }  
-                
-                // Increase best sequence length &  
-                // save next number in our triplet 
-                else if (nums[i] > max_seq)  
-                {     
-                seq++; 
-                    
-                // We've found our arr[k]! 
-                // Print the output 
-                if (seq == 3)  
-                {             
-                    System.out.println("Triplet: " + store_min + ", " + max_seq + ", " + nums[i]); 
-                    return true; 
-                } 
-                max_seq = nums[i]; 
-                } 
-            } 
-            return false;
+            int l= nums.length;
+            int[] left= new int[l];
+            int[] right= new int[l];
+            left[0]= nums[0];
+            for(int i=1;i<l;i++){ //Find left min
+                left[i]=Math.min(nums[i], left[i-1]);
+            }
+            right[l-1]= nums[l-1];
+            for(int i=l-2;i>=0;i--){ //Find right max
+                right[i]=Math.max(nums[i], right[i+1]);
+            }
+            for(int i=0;i<l;i++){ 
+                if(left[i]< nums[i] && nums[i]< right[i]) 
+                    return true;
+            }
+         return false;
 
             /*
             int n=nums.length;
