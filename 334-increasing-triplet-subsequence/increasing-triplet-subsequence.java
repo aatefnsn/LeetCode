063a1dd17aa79b4ea83076d27ea30aa8d2dec8f1
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2024-09-11 19:27:31 -0700

    Time: 2 ms (89.49%) | Memory: 133.8 MB (61.04%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,25 +1,58 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if (nums.length <3){
+        int n = nums.length;
+        int f = Integer.MAX_VALUE, s = Integer.MAX_VALUE;
+        for (int nValue : nums) {
+            //System.out.println("value is " + nValue + ", s is "+ s + ",f is "+ f);
+            if (nValue <= f) {
+                //System.out.println("first if, value is " + nValue + " and f (before) is " + f);
+                f = nValue;
+                //System.out.println("first if, value is " + nValue + " and f (after) is " + f);
+            }else if (nValue <= s) {
+                //System.out.println("second if, value is " + nValue + " and s (before) is " + s);
+                s = nValue;
+                //System.out.println("second if, value is " + nValue + " and f is " + f);
+                //System.out.println("second if, value is " + nValue + " and s (after) is " + s);
+            }else if (nValue > s) {
+                //System.out.println("found third, value is " + nValue);
+                return true;
+            }
+        }
+        return false;
+////////////////////////
+        // time limit exceeded
+        /*if (nums.length <3){
             return false;
         }else{
-            int l= nums.length;
-            int[] left= new int[l];
-            int[] right= new int[l];
-            left[0]= nums[0];
-            for(int i=1;i<l;i++){ //Find left min
-                left[i]=Math.min(nums[i], left[i-1]);
+            for (int i=0; i<nums.length; i++){
+                Boolean has_second=false;
+                int first=nums[i];
+                int second_index=i;
+                int second=Integer.MAX_VALUE;
+                //System.out.println("first is " + first);
+                for(int j=i+1; j<nums.length;j++){
+                    if (nums[j] > first && has_second == false){
+                        second=nums[j];
+                        //System.out.println("second is " + second);
+                        has_second=true;
+                        second_index=j;
+                        //System.out.println("second index is " + second_index);                 
+                    }
+                    if(nums[j] > second && has_second == true){
+                        //System.out.println("third is "+ nums[j]);
+                        return true;
+                    }
+                    if(j==nums.length-1 && has_second==true){
+                        //System.out.println("reached dead end and could not find third, need to find next second");
+                        j=second_index;
+                        has_second=false;
+                    }
+                    
+                }
             }
-            right[l-1]= nums[l-1];
-            for(int i=l-2;i>=0;i--){ //Find right max
-                right[i]=Math.max(nums[i], right[i+1]);
-            }
-            for(int i=0;i<l;i++){ 
-                if(left[i]< nums[i] && nums[i]< right[i]) 
-                    return true;
-            }
-         return false;
-
+           return false;*/
+/////////////////////////////////////////////////////////////
+            // not working solution
             /*
             int n=nums.length;
             for(int i=0; i<n-1; i++){
@@ -35,6 +68,8 @@ class Solution {
                 }
             }
             return false;*/
+////////////////////////////////////////////////////////////////////////////////////////////////
+//          time limit exceeded
 
             /*int n = nums.length;
             for(int i=0;i<n-2;i++){
@@ -52,4 +87,4 @@ class Solution {
             return false;*/
         }
     }
-}
+//}
