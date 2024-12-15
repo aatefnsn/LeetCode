d28bab67d4bff792bfb8873c1dc68af5111a9982
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2024-12-15 14:20:31 -0800

    Time: 7 ms (96.47%), Space: 13.8 MB (33.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,10 +1,24 @@
 import re
+
+import re
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+        if len(s) > 0:
+            s=re.sub(r'[^A-Za-z0-9]','',s).lower()
+            start=0
+            end=len(s)-1
+            #print(end)
+            print(s)
+            while(start < end):
+                if s[start] == s[end]:
+                    start=start+1
+                    end=end-1
+                else:
+                    return False
+        else:
+            return False
+        return True
+'''
         if len(s) > 0:
             s=re.sub('[^A-Za-z0-9]*','',s).lower()
             start=0
@@ -20,3 +34,4 @@ class Solution(object):
                     return False
            
         return True
+'''
