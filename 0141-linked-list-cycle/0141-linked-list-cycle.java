a03806908c41810fed4da6803d8715c4d89def26
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2025-01-23 11:21:27 -0800

    Time: 0 ms (100%), Space: 44.2 MB (86.82%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+
+    
+    public boolean hasCycle(ListNode head) {
+        ListNode walker=head;
+        ListNode runner=head;
+        if (head == null){
+            return false;
+        }
+        while (runner.next != null && runner.next.next != null){
+            walker=walker.next;
+            runner=runner.next.next;
+            if(runner == walker){
+                return true;
+            }
+        }
+        return false;
+        /*
+        ListNode walker = head;
+        ListNode runner = head;
+        if (head == null){
+            return false;
+        }
+        while (runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (runner == walker) {
+                return true;
+            }
+            
+        }
+        return false;
+        */
+    }
+}
