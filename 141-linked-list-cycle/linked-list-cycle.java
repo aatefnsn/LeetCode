56b27c4e5023f46128207cf24b31d5123b2f36c1
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2025-01-23 11:22:43 -0800

    Time: 0 ms (100.00%) | Memory: 44.5 MB (61.78%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -13,17 +13,11 @@ public class Solution {
 
     
     public boolean hasCycle(ListNode head) {
-        ListNode walker=head;
-        ListNode runner=head;
-        if (head == null){
-            return false;
-        }
-        while (runner.next != null && runner.next.next != null){
-            walker=walker.next;
-            runner=runner.next.next;
-            if(runner == walker){
-                return true;
-            }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
         }
         return false;
         /*
