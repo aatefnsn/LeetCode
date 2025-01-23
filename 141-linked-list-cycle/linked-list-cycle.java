592211fadd3f05fae4df62391d2b051746d7843a
aatefnsn <48165450+aatefnsn@users.noreply.github.com>
2025-01-23 11:02:26 -0800

    Time: 0 ms (100.00%) | Memory: 44.2 MB (86.82%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -13,18 +13,17 @@ public class Solution {
 
     
     public boolean hasCycle(ListNode head) {
-        ListNode walker = head;
-        ListNode runner = head;
+        ListNode walker=head;
+        ListNode runner=head;
         if (head == null){
             return false;
         }
-        while (runner.next != null && runner.next.next != null) {
-            walker = walker.next;
-            runner = runner.next.next;
-            if (runner == walker) {
+        while (runner.next != null && runner.next.next != null){
+            walker=walker.next;
+            runner=runner.next.next;
+            if(runner == walker){
                 return true;
             }
-            
         }
         return false;
         /*
