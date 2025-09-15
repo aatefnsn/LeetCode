4d4ee62415ee2cb84c2ee0b60cf083a5e4555e1d
Ahmed A. Nada <48165450+aatefnsn@users.noreply.github.com>
2025-09-14 17:30:22 -0700

    Time: 6 ms (51.7%), Space: 18.9 MB (82.75%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,7 +1,35 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        PreMap=defaultdict(list)
+        courseDepCount= [0] * numCourses
+
+        for course, prereq in prerequisites:
+            PreMap[prereq].append(course)
+            courseDepCount[course]+=1
+        
+        queue=deque()
+
+        for i in range(numCourses):
+            if courseDepCount[i] == 0:
+                queue.append(i)
+        
+        completed=0
+
+        while queue:
+            prereq=queue.popleft()
+            completed+=1
+            for nextCourse in PreMap[prereq] :
+                courseDepCount[nextCourse]-=1
+                if courseDepCount[nextCourse] == 0:
+                    queue.append(nextCourse)
+
+        return completed == numCourses
+
+        
+        """
         #def canFinish(numCourses, prerequisites):
-    # Build adjacency list and calculate in-degrees
+        # Build adjacency list and calculate in-degrees
         graph = defaultdict(list)
         in_degree = [0] * numCourses
         
@@ -26,4 +54,4 @@ class Solution:
         
         # If we completed all courses, no cycle exists
         return completed == numCourses
-        
+        """
