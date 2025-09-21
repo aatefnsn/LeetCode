942d70f123f03bc801b85bdd33fa0f20709c7307
Ahmed A. Nada <48165450+aatefnsn@users.noreply.github.com>
2025-09-21 12:29:06 -0700

    Time: 218 ms (1.27%), Space: 17.9 MB (37.83%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -12,17 +12,17 @@ class Solution:
         elif not list2 and list1:
             return list1
         else:
-            print("2 non empty lists")
+            #print("2 non empty lists")
             r = ListNode()
             cur = r
             while list1 and list2:
                 if list1.val <= list2.val:
-                    print("list1 val smaller")
+                    #print("list1 val smaller")
                     cur.val = list1.val
                     list1=list1.next
                     print(r)
                 else:
-                    print("list2 val smaller")
+                    #print("list2 val smaller")
                     cur.val = list2.val
                     print(r)
                     list2=list2.next
@@ -36,8 +36,5 @@ class Solution:
                 cur.val=list2.val
                 list2=list2.next
                 cur.next=list2
-                #make a new ListNode, assign it the value
-                # set it as either root of the new list or direct existing node .next to the new ListNode
-            
         return r
         
