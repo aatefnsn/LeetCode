2c4e0b53e9da1cfdeb39e08dcf5d3b3f72b8e30d
Ahmed A. Nada <48165450+aatefnsn@users.noreply.github.com>
2025-09-29 20:46:32 -0700

    Time: 3 ms (15.69%) | Memory: 19.4 MB (29.44%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.py
+++ 328-odd-even-linked-list/odd-even-linked-list.py
@@ -8,25 +8,14 @@ class Solution:
         if not head:
             return
         else:
-            count=0
             new_head=ListNode(head.val)
-            #print("new head",new_head.val)
-            #new_head2=head
-            #new_head.next= None
             ans=new_head
-            #new_head=new_head.next
             curOdd=head
-            #print("curOdd is ", curOdd.val)
-            #print("curOdd next should be ", curOdd.next.next.val)
             while curOdd.next and curOdd.next.next:
                 curOdd=curOdd.next.next
-                print(curOdd.val)
                 new_head.next=ListNode(curOdd.val)
-                #print("new head ")
-                #new_head.next.next= None
                 new_head=new_head.next
-                
-            #print("done with odd")
+
             curEven=head.next
             if curEven:
                 new_head.next=ListNode(curEven.val)
@@ -35,14 +24,4 @@ class Solution:
                     curEven=curEven.next.next
                     new_head.next=ListNode(curEven.val)
                     new_head=new_head.next
-
-                #curEven=head.next
-                #print(curEven.val)
-                #new_head.next=curEven
-                '''
-                while curEven.next and curEven.next.next:
-                    curEven=curEven.next.next
-                    new_head.next=curEven
-                    new_head=new_head.next
-                '''
         return ans
