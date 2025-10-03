d2497f5e045916aed4759c4b5d50d7b2da7a21d1
Ahmed A. Nada <48165450+aatefnsn@users.noreply.github.com>
2025-10-02 19:54:50 -0700

    Time: 0 ms (100.00%) | Memory: 19 MB (47.55%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -14,23 +14,18 @@ class Solution:
         if not root:
             return 0
         else:
-            #count=1
             stack.appendleft((root,1))
-            depths[root]=1
+            #depths[root]=1
             maxDepth=1
             while stack:
                 cur,curDepth=stack.popleft()
                 maxDepth=max(curDepth, maxDepth)
-                #count+=1
-                #print(cur.val)
-                #visited.append(cur)
-                #visited_level[cur]=count
                 
                 if cur.left:
                     stack.append((cur.left, curDepth+1))
-                    depths[cur.left]=curDepth+1
+                    #depths[cur.left]=curDepth+1
                 if cur.right:
                     stack.append((cur.right,curDepth+1))
-                    depths[cur.right]=curDepth+1
+                    #depths[cur.right]=curDepth+1
             return maxDepth
 
